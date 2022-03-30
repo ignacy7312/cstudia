05c14344dc52b92a0463dc3e201fd2030bf948e5
pochodyla <pochodyla@gmail.com>
2022-03-30 23:25:07 +0200

    update 30.03

---
+++ codeforces/stonesonthetable.cpp
@@ -0,0 +1,17 @@
+//https://codeforces.com/problemset/problem/266/A
+#include <bits/stdc++.h>
+
+int main(void){
+
+    int total;
+    
+    std::cin >> total;
+    char colors[total];
+    std::cin >> colors;
+
+    std::cout << colors[0];
+
+
+
+    return 0;
+}
